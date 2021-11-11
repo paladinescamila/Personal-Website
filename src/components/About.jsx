e34ab9ab50c868756d32d263662be11c84fb9290
paladinescamila <camilapaladines27@gmail.com>
2021-11-11 17:45:49 -0500

    Added new skill to about sectio

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,8 +14,8 @@ export default function About() {
 			</p>
 			<div className='about-skills'>
 				<SkillCard image={languagesImage} title='Languages' tools='HTML, CSS, SCSS, JavaScript, Python' />
-				<SkillCard image={librariesImage} title='Libraries & Frameworks' tools='React JS' />
-				<SkillCard image={toolsImage} title='Tools & Platforms' tools='Git, Netlify, Firebase' />
+				<SkillCard image={librariesImage} title='Libraries & Frameworks' tools='React.JS' />
+				<SkillCard image={toolsImage} title='Tools & Platforms' tools='Git, Netlify, Firebase, Figma' />
 			</div>
 		</div>
 	);

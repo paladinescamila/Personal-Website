04aeaa44b9d458aadb4980a84edc8df13bbcc6e7
paladinescamila <camilapaladines27@gmail.com>
2021-12-17 20:41:43 -0500

    Added scroll animation

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,21 +1,27 @@
 import React from "react";
 import Button from "./Button";
 import launch from "../img/launch.svg";
+import scroll from "../img/scroll-down.svg";
 
 export default function Home({content}) {
 	return (
-		<div className='home-section home-padding'>
-			<div className='home-left'>
-				<div className='home-hi'>
-					<p>{content.hi}</p>
-					<p>Camila Paladines</p>
-					<p>{content.job}</p>
+		<>
+			<div className='home-section home-padding'>
+				<div className='home-left'>
+					<div className='home-hi'>
+						<p>{content.hi}</p>
+						<p>Camila Paladines</p>
+						<p>{content.job}</p>
+					</div>
+					<div>
+						<Button value={content.button} type='solid' link='mailto:camilapaladines27@gmail.com' />
+					</div>
 				</div>
-				<div>
-					<Button value={content.button} type='solid' link='mailto:camilapaladines27@gmail.com' />
-				</div>
+				<img src={launch} alt='Frontend'></img>
 			</div>
-			<img src={launch} alt='Frontend'></img>
-		</div>
+			<div className='scroll-down'>
+				<img src={scroll} alt='Scroll down'></img>
+			</div>
+		</>
 	);
 }

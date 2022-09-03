76a4c05ce238dc7844087e5439d310860da0bc29
Camila Paladines <38636399+paladinescamila@users.noreply.github.com>
2022-09-02 22:45:18 -0500

    Update jobs.js

--- src/Services/Constants/jobs.js
+++ src/Services/Constants/jobs.js
@@ -1,18 +1,20 @@
 const jobs = [
 	{
 		companyName: "Ninus Design & Technology",
-		companyLink: "https://ninus.co/",
+		companyLink: "https://ninus.co",
 		date: [new Date(2022, 2, 18), new Date()],
 		positionEN: "FRONTEND DEVELOPER",
 		positionES: "DESARROLLADORA FRONTEND",
 		tasksEN: [
-			"Web design and implementation of client interaction functionalities using the React.js library.",
-			"Consumption of REST APIs to obtain information from the Backend.",
-			"Debugging and testing of the developed functionalities.",
+			"Web design and implementation of client interaction functionalities using React.js and TypeScript.",
+			"Implementation of a mobile application using React Native, TypeScript, SCSS and Firebase services.",
+			"REST type APIs consumption.",
+			"Debugging and testing of developed functionalities.",
 		],
 		tasksES: [
-			"Diseño web e implementación de funcionalidades de la interacción con el cliente usando la librería React.js.",
-			"Consumo de APIs tipo REST para la obtención de información desde el Backend.",
+			"Diseño web e implementación de funcionalidades de la interacción con el cliente usando React.js y TypeScript.",
+			"Implementación de una aplicación móvil usando React Native, TypeScript, SCSS y servicios de Firebase.",
+			"Consumo de APIs tipo REST.",
 			"Depuración y pruebas de las funcionalidades desarrolladas.",
 		],
 	},
